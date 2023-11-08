4007b9b5631b3a0f673794251b12200dbbd09841
Sona Sreedhar <sonasreedhar44@gmail.com>
2023-11-08 14:53:26 +0530

    check redux working

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import Header from './containers/Header'
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import ProductListing from './containers/ProductListing'
+import ProductDetail from './containers/ProductDetail'
+
+function App() {
+  return (
+ <div className='App'>
+  <Router>
+    <Header/>
+    <Routes>
+    <Route path='/' exact Component={ProductListing}/>
+    <Route path='/product/:productId' exact component={ProductDetail}/>
+    <Route>404 Not Found!</Route>
+    </Routes>
+    
+  </Router>
+ </div>
+  )
+}
+
+export default App
