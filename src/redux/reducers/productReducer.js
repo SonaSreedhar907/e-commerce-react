6958e150110ed97a0232a6a836ff414fedc2cf3a
Sona Sreedhar <sonasreedhar44@gmail.com>
2023-11-08 16:17:17 +0530

    dispatch added

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -2,18 +2,14 @@ import { ActionTypes } from "../contants/action-types";
 
 
 const initialState={
-    products:[{
-        id:1,
-        name:'akhil',
-        job:'doctor'
-    }]
+    products:[]
 }
 
 
 export const productReducer=(state=initialState,{type,payload})=>{
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return state
+            return {...state,products:payload}
         default:
             return state
     }
